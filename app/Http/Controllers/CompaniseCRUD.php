5f59bc506e98c975ca16acfded45bc74cfbc5ee6
Sumate Kaewjai <63050203@kmitl.ac.th>
2022-10-22 20:12:58 +0700

    'CRUD'

---
+++ app/Http/Controllers/CompaniseCRUD.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Company;
+use Illuminate\Http\Request;
+
+
+class CompaniseCRUD extends Controller
+{
+    //Create Index
+    public function index(){
+        $data['companies'] = Company::orderBy('id','asc')->paginate(5);
+        return view('companies.index',$data);
+    }
+    //Create resource
+    public function create() {
+        return view('companies.create');
+    }
+    
+    public function store(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'address' => 'required'
+        ]);
+        $company = new Company;
+        $company->name = $request->name;
+        $company->email = $request->email;
+        $company->address = $request->address;
+        $company->save();
+        return redirect()->route('companies.index')->with('success','create success');
+    }
+
+    public function edit(Company $company){
+        return view('companies.edit',compact('company'));
+    }
+
+    public function update(Request $request,$id){
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'address' => 'required',
+        ]);
+        $company = Company::find($id);
+        $company->name = $request->name;
+        $company->email = $request->email;
+        $company->address = $request->address;
+        $company->save();
+        return redirect()->route('companies.index')->with('success',"Company has been update success");
+        
+    }
+
+    public function destroy (Company $company){
+        $company->delete();
+        return redirect()->route('companies.index')->with('success',"Company has been deleted success");
+        
+    }
+}
